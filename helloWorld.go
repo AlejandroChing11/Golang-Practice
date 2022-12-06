f059fd6267d55f9e70b4765a3a3533334bdf77c9
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-06 12:39:24 -0500

    Adding comments

--- helloWorld.go
+++ helloWorld.go
@@ -1,8 +1,6 @@
-package main
+package main //Central point - package "#principal - data name"
 
-import (
-	"fmt"
-)
+import "fmt" //it can be with brackets -- fmt is a package
 
 func main() {
 	fmt.Println("My first hello world with Golang/Go")
