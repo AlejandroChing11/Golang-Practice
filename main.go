79fc8ca0a1579c22e5c0668879a513602dd43536
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-06 13:51:10 -0500

    Learning variables, const and zero values

--- main.go
+++ main.go
@@ -14,4 +14,16 @@ func main() {
 	var altura int = 14 //We got to type the data type explicitly
 	var ancho int       //The value of a variable that is initialized but haven't been declared yet is 0.
 	fmt.Println(base, altura, ancho)
+	//zero values
+	var a int     //Integer value
+	var b string  //String value
+	var d bool    //True-False boolean
+	var c float64 //Float64 value
+	fmt.Println(a, b, c, d)
+
+	//Area de un cuadrado
+	const baseCuadrado = 12 //camelCase
+	const alturaCuadrado = 14
+	area := baseCuadrado * alturaCuadrado //We use ":=" because we used other variables that were already declared
+	fmt.Println("La base del cuadrado es: ", baseCuadrado, "La altura del cuadrado es: ", alturaCuadrado, "Y el area del cuadrado es: ", area)
 }
